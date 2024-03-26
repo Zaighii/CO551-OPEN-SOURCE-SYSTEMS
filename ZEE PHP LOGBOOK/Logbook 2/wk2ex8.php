12439e69413911ec1bd7dd4615f0def32525f37f
Zaigham Khan <126190452+Zaighii@users.noreply.github.com>
2024-03-26 22:15:28 +0000

    Add files via upload

---
+++ ZEE PHP LOGBOOK/Logbook 2/wk2ex8.php
@@ -0,0 +1,13 @@
+<?php
+$topModules[0] = "Internet Systems Develpment";
+$topModules[1] = "Programming 1";
+$topModules[2] = "Programming 2";
+$topModules[3] = "OOSD";
+$topModules[4] = "Software Engineering";
+$topModules[5] = "Data Structures";  // Added module
+$topModules[6] = "Database Managment";  // Added module
+
+for ($count = 0; $count < 7; $count++) {
+    echo "$count module is $topModules[$count] <br/>";
+}
+?>
